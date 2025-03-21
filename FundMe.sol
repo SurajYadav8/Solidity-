9ff1f56f0ab18d5bae4f98d4f2534036b9889b35
SurajYadav8 <raosahab1e@gmail.com>
2025-03-21 23:40:41 +0530

    fundMe

--- FundMe.sol
+++ FundMe.sol
@@ -5,40 +5,36 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-interface AggregatorV3Interface {
-  function decimals() external view returns (uint8);
-
-  function description() external view returns (string memory);
-
-  function version() external view returns (uint256);
-
-  function getRoundData(
-    uint80 _roundId
-  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
-
-  function latestRoundData()
-    external
-    view
-    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
-}
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
 
-    uint256 minimumUsd = 5;
+    address[] public Funders;
+    mapping(address => uint256 ) public addressToAmountFunders;
+    
+    uint256 minimumUsd = 5e18;
 
     function fund() public payable {
-        require(msg.value > minimumUsd, "didn't have enough amount");
+        require( conversion(msg.value) > minimumUsd, "didn't have enough amount");
+        Funders.push(msg.sender);
+        addressToAmountFunders[msg.sender] = addressToAmountFunders[msg.sender] + msg.value;
     }
 
     function withdraw() public {
 
     }
     
-    function getPrice () public {
+    function getPrice () public view returns(uint256) {
         //address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        AggregatorV3Interface pricefeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int256 price, , ,) = pricefeed.latestRoundData();
+
+        return uint256(price * 1e10);
     }
-    function conversion () public {
-        
+    function conversion (uint256 ethAmount) public view returns(uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAountUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAountUsd;
     }
     function getVersion() public view returns(uint256){
         return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
