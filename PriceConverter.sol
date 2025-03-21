f3ea55e8a9915b067b694eb5cd4616beccc98d90
SurajYadav8 <raosahab1e@gmail.com>
2025-03-22 00:06:37 +0530

    library update

--- PriceConverter.sol
+++ PriceConverter.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.18;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
-library priceConverter {
+library PriceConverter {
        
     function getPrice () public view returns(uint256) {
         //address 0x694AA1769357215DE4FAC081bf1f309aDC325306
