c41ba1508b204e098a29c64861db895a0f71f3f7
SurajYadav8 <raosahab1e@gmail.com>
2025-03-22 00:04:51 +0530

    library update

--- FundMe.sol
+++ FundMe.sol
@@ -5,9 +5,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
-
+import {PriceConverter} from "./PriceConverter.sol";
 contract FundMe {
+
+    using PriceConverter for uint256;
 
     address[] public Funders;
     mapping(address => uint256 ) public addressToAmountFunders;
@@ -15,7 +16,7 @@ contract FundMe {
     uint256 minimumUsd = 5e18;
 
     function fund() public payable {
-        require( conversion(msg.value) > minimumUsd, "didn't have enough amount");
+        require(msg.value.conversion() > minimumUsd, "didn't have enough amount");
         Funders.push(msg.sender);
         addressToAmountFunders[msg.sender] = addressToAmountFunders[msg.sender] + msg.value;
     }
@@ -24,19 +25,4 @@ contract FundMe {
 
     }
     
-    function getPrice () public view returns(uint256) {
-        //address 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        AggregatorV3Interface pricefeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (,int256 price, , ,) = pricefeed.latestRoundData();
-
-        return uint256(price * 1e10);
-    }
-    function conversion (uint256 ethAmount) public view returns(uint256){
-        uint256 ethPrice = getPrice();
-        uint256 ethAountUsd = (ethPrice * ethAmount) / 1e18;
-        return ethAountUsd;
-    }
-    function getVersion() public view returns(uint256){
-        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
-    }
 }
