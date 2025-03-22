232cb7e02a98995cfafac1cd3df5781bafa5d4d8
SurajYadav8 <raosahab1e@gmail.com>
2025-03-22 19:57:28 +0530

    safemath

--- FundMe.sol
+++ FundMe.sol
@@ -11,18 +11,22 @@ contract FundMe {
     using PriceConverter for uint256;
 
     address[] public Funders;
-    mapping(address => uint256 ) public addressToAmountFunders;
+    mapping(address => uint256 ) public addressToAmountFunded;
     
     uint256 minimumUsd = 5e18;
 
     function fund() public payable {
         require(msg.value.conversion() > minimumUsd, "didn't have enough amount");
         Funders.push(msg.sender);
-        addressToAmountFunders[msg.sender] = addressToAmountFunders[msg.sender] + msg.value;
+        addressToAmountFunded[msg.sender]  +=  msg.value;
     }
 
     function withdraw() public {
-
+        //for loop 
+        for(uint256 i = 0; i < Funders.length; i++){
+            address funder = Funders[i];
+            addressToAmountFunded[funder] = 0;
+        }
     }
     
 }
