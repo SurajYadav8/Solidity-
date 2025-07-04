eed377675ec7cb7d66f36fdd666b3b46e967574e
SurajYadav8 <raosahab1e@gmail.com>
2025-07-04 23:01:03 +0530

    calls

--- CallAnything.sol
+++ CallAnything.sol
@@ -13,7 +13,6 @@
 pragma solidity 0.8.7;
 
 contract CallAnything {
-
     address public s_someAddress;
     uint256 public s_amount;
 
@@ -22,8 +21,39 @@ contract CallAnything {
         s_amount = amount;
     }
 
-    function getselectorOne() public pure returns ( bytes4 selector) {
-        selector = bytes4(keccak256(bytes("transfer(address,uint256")));
+    function getselectorOne() public pure returns (bytes4 selector) {
+        selector = bytes4(keccak256(bytes("transfer(address,uint256)")));
+    }
+
+    function getDataToCallTransfer(address someAddress, uint256 amount)
+        public
+        pure
+        returns (bytes memory)
+    {
+        return abi.encodeWithSelector(getselectorOne(), someAddress, amount);
+    }
+
+    function callTransferFunctionDirectly(address someAddress, uint256 amount)
+        public
+        returns (bytes4, bool)
+    {
+        (bool success, bytes memory returnData) = address(this).call(
+            // getDataToCallTransfer(someAddress, amount)
+
+            abi.encodeWithSelector(getselectorOne(), someAddress, amount)
+        );
+        return (bytes4(returnData), success);
+    }
+
+     function callTransferFunctionDirectlySig(address someAddress, uint256 amount)
+        public
+        returns (bytes4, bool)
+    {
+        (bool success, bytes memory returnData) = address(this).call(
+            // getDataToCallTransfer(someAddress, amount)
+
+            abi.encodeWithSignature("transfer(address,uint256)", someAddress, amount)
+        );
+        return (bytes4(returnData), success);
     }
 }
-
