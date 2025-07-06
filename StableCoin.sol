5c1b75927b9ce3f142f57dae1443019efe7133ee
SurajYadav8 <raosahab1e@gmail.com>
2025-07-06 23:44:28 +0530

    Defi

---
+++ StableCoin.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+//stablecoin is a crypto asset whose buying power fluctuates very little Relative to the rest of the market
+
+//Types of StableCoins
+//   1. Relatvie Stability (Pegged, Anchored or Floating )
+    //  2. Stability Method (Governed or algorithmic) 
+
+contract stablecoin {
+
+}
